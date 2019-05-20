f0128fae7dfc4168dbc3d9f622c0a73aa2153ca7
Juan <jm.munoz14@uniandes.edu.co>
2019-05-20 11:01:31 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,8 +1,9 @@
 
 // check env.
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'production';
 // fetch env. config
 var config = require('./config.json');
 var envConfig = config[env];
 // add env. config values to process.env
 Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
+console.log("todo bien hasta aqui")
